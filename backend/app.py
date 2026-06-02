8438875f56716c19c9fc1edbed99b21e5836db7b
DAVID0836 <l21590390@sjuanrio.tecnm.mx>
2026-06-02 13:27:31 -0600

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,14 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 from config import get_connection
 import datetime
 
 app = Flask(__name__)
 
-CORS(app, resources={r"/*": {"origins": "*"}})
-# actualizando cors
-
+CORS(app, support_credentials=True)
 
 @app.route('/login', methods=['POST'])
+@cross_origin(supports_credentials=True)
 def login():
     data = request.get_json()
     usuario = data.get('user')
